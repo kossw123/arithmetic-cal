878a24b4d55e219f5eab4cc59fc48e225009fed1
kossw123 <kossw123@naver.com>
2023-01-31 10:40:04 +0900

    commit

--- CustomEvent.cs
+++ CustomEvent.cs
@@ -24,8 +24,8 @@ public class MainClass<T>
     }
 }
 
-
-public class State<T> : EventArgs
+/// IDisposable 구현
+public class State<T> : EventArgs 
 {
     public enum FlagState
     {
@@ -53,18 +53,26 @@ public class State<T> : EventArgs
         }
     }
 
+    /// static State(T input) 을 사용하여 인스턴스 생성자보다 더 빠르게 콜
+    /// 그리고 딱 한번만 호출
     public State(T input) => this.data = input;
 
     public static implicit operator State<T>(T value) => new State<T>(value);
     public static implicit operator T(State<T> value) => value.Data is not null ? (T)Convert.ChangeType(value.Data, typeof(T)) : throw new Exception(nameof(value));
-}
-public class Behaviour
+    public class Behaviour
 {
+    /// 이 부분을 evnet 원형으로 설정하던지
+    /// public delegate void CustomEvnetHandler(object sender, EvnetArgs e);
+    /// public evnet CustomEvnetHandler handler;
     public event EventHandler<EventArgs> behaviour;
+
     public void AddBehaviour(Action<object, EventArgs> e)
     {
         if(e is not null)
         {
+            /// 이 부분 refactoring
+            /// 2번의 shallow copy
+            /// 혹은 invoke
             Action<object, EventArgs> temp = e.Invoke;
             EventHandler<EventArgs> h = temp.Invoke!;
             behaviour += h;
